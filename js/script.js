43f9cc654a23d5bb3c4f1db9be5ee92081a0c5d5
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-26 17:11:54 +0200

    [UPD] insert-variable-age-and-km

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Realizzare una variabile alla quale assegno un prompt per chiedere all'utente i km che dovrà percorrere//
+let km = prompt('Inserisci i km da percorrere');
+
+//Realizzare una variabile alla quale assegno un prompt per chiedere all'utente la sua età //
+let eta = prompt('Inserisci la tua età');
