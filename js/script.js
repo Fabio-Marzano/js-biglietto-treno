2011886fa9ea024d2b46647e493f6685cc360fd2
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-26 17:15:59 +0200

    [UPD] insert-price-based-in-console

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ let km = prompt('Inserisci i km da percorrere');
 
 //Realizzare una variabile alla quale assegno un prompt per chiedere all'utente la sua età //
 let eta = prompt('Inserisci la tua età');
+
+//Realizzo variabile che contiene il prezzo base//
+let prezzo_base = km * 0.21;
+console.log(prezzo_base);
